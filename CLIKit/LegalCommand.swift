824df2688fc15f15a06c3f579719ea8098b054c9
Drew Crawford <drew@sealedabstract.com>
2015-08-18 18:22:22 -0500

    License: RPL

--- CLIKit/LegalCommand.swift
+++ CLIKit/LegalCommand.swift
@@ -4,11 +4,19 @@
 //
 //  Created by Drew Crawford on 8/18/15.
 //  Copyright © 2015 DrewCrawfordApps. All rights reserved.
-//  This file is part of CLIKit.  It is subject to the license terms in the LICENSE
-//  file found in the top level of this distribution
-//  No part of CLIKit, including this file, may be copied, modified,
-//  propagated, or distributed except according to the terms contained
-//  in the LICENSE file.
+//  CLIKit © 2015 DrewCrawfordApps LLC
+//
+//  Unless explicitly acquired and licensed from Licensor under another
+//  license, the contents of this file are subject to the Reciprocal Public
+//  License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+//  and You may not copy or use this file in either source code or executable
+//  form, except in compliance with the terms and conditions of the RPL.
+//  All software distributed under the RPL is provided strictly on an "AS
+//  IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+//  LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+//  LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+//  PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+//  language governing rights and limitations under the RPL.
 
 import Foundation
 /**A command that outputs legal text. */
